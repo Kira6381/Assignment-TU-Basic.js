5cee524831b5eb476f006c18d7a12754dd8ed359
Zrital <Ztrital12580@gmail.com>
2024-05-08 11:43:00 +0545

    finished up yee

--- Movie-app/script.js
+++
@@ -1,5 +0,0 @@
-// Add your JavaScript code here
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM loaded");
-  // You can start writing your JavaScript code here
-});
