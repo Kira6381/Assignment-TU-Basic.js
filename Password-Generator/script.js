74a850d5440cd19191715f7af59037b86df42b58
Zrital <Ztrital12580@gmail.com>
2024-05-08 11:00:22 +0545

    pass gen done

--- Password-Generator/script.js
+++ Password-Generator/script.js
@@ -1,5 +1,28 @@
-// Add your JavaScript code here
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM loaded");
-  // You can start writing your JavaScript code here
+  const lengthInput = document.getElementById("length");
+  const generateBtn = document.getElementById("generateBtn");
+  const passwordInput = document.getElementById("password");
+  const copyBtn = document.getElementById("copyBtn");
+
+  generateBtn.addEventListener("click", generatePassword);
+  copyBtn.addEventListener("click", copyPassword);
+
+  function generatePassword() {
+    const length = parseInt(lengthInput.value);
+    const charset =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+";
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+
+    passwordInput.value = password;
+  }
+
+  function copyPassword() {
+    passwordInput.select();
+    document.execCommand("copy");
+  }
 });
