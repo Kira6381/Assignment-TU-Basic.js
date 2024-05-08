39cb0211540f5af7485dcbbaec20cc035518f15e
Zrital <Ztrital12580@gmail.com>
2024-05-08 10:55:40 +0545

    drum-kit app done

---
+++ Movie-app/script.js
@@ -0,0 +1,5 @@
+// Add your JavaScript code here
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM loaded");
+  // You can start writing your JavaScript code here
+});
